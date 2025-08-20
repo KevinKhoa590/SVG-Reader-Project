c6b34cd152a2dd4aaf1858d44781bee1985ab521
GaMatter206 <nguyenphucbaophuc@gmail.com>
2025-08-20 21:08:25 +0700

    Update SVGText.cpp

--- SVG-Reader/SVGReader/SVGText.cpp
+++ SVG-Reader/SVGReader/SVGText.cpp
@@ -7,13 +7,24 @@
 using namespace Gdiplus;
 
 SVGText::SVGText(int x, int y, int fontSize, const Color* fill, const std::string& text)
-    : x(x), y(y), fontSize(fontSize), fill(fill), text(text) {}
+    : x(x), y(y), fontSize(fontSize), fillColor(fill), text(text) {}
 
-void SVGText::draw(Graphics& graphics) {
+void SVGText::draw(Gdiplus::Graphics& graphics) {
+    RectF boundsF(static_cast<REAL>(x), static_cast<REAL>(y - fontSize), static_cast<REAL>(fontSize * text.size()), static_cast<REAL>(fontSize));
+
+    auto brushPtr = createFillBrush(graphics, boundsF);
     FontFamily fontFamily(L"Arial");
     Font font(&fontFamily, static_cast<REAL>(fontSize), FontStyleRegular, UnitPixel);
-    SolidBrush brush(*fill);
-
     std::wstring wtext(text.begin(), text.end());
-    graphics.DrawString(wtext.c_str(), -1, &font, PointF(static_cast<REAL>(x), static_cast<REAL>(y)), &brush);
+    PointF pt(static_cast<REAL>(x), static_cast<REAL>(y));
+
+    if (brushPtr) {
+        graphics.DrawString(wtext.c_str(), -1, &font, pt, brushPtr.get());
+    }
+    else if (fillColor) {
+        SolidBrush brush(*fillColor);
+        graphics.DrawString(wtext.c_str(), -1, &font, pt, &brush);
+    }
 }
+
+
